fdf8627d0f83d8cbb44eff5b4a5e0469387359ba
SHiuN26 <justodme26@gmail.com>
2024-06-30 16:54:51 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import { Todo } from './interfaces/Todo'
+import { Button } from 'antd'
+
+
+const App:React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([])
+  return (
+    <>
+      總代辦事項數量: {todos.length}
+    </>
+  )
+}
+
+export default App
